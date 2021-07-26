706bf58ca25843eec646c6b5017222e3ab143578
Bob Skowron <bob.skowron@gmail.com>
2021-07-26 15:04:43 -0500

    Finished up module 1.1

--- README.md
+++ README.md
@@ -2,9 +2,13 @@
 
 Code working through the HW and other problem sets from Spring 2021: https://computationalthinking.mit.edu/Spring21/
 
+While the lectures are done with Pluto notebooks, I'm just playing with the code in files.
+
 ## Helpful Tidbits
 
 ### Package Management
 ```]``` enters into the pkg REPL
+
 ```activate .``` creates a new local .toml environment
+
 ```add <packagename>``` installs the relevant package
--- module-1/1-1.jl
+++ module-1/1-1.jl
@@ -13,3 +13,41 @@ philip = load(philip_filename)
 test_filename = downlad("https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png")
 # download a file that is already on your own computer
 test_load = load("./module-1/automation.png")
+
+# Inspecting your data
+philip_size = size(philip)
+philip_height = philip_size[1]
+philip_width = philip_size[2]
+
+a_pixel = philip[200, 100]
+row_i,col_i=(1,1)
+
+philip[550:650, 1:philip_width] # vertical 550-650, all horizontal
+philip[550:650, :] # equivalent 
+
+philip[550, :] # single row
+
+philip_head = philip[470:800, 140:410] # headshot
+
+# Modifying an image
+# Exercise 2.5
+# Write a function invert that inverts a color, i.e. sends (r,g,b) to (1-r, 1-g, 1-b)
+function invert(color::AbstractRGB)
+    return RGB(1.0-color.r, 1.0-color.g, 1.0-color.b)
+end
+
+philip_inverted = invert.(philip)
+
+
+# Exercise 1.2
+# Generate a vector of 100 zeros. Change the center 20 elements to 1.
+function create_bar()
+	z_vec = zeros(100)
+    z_vec[41:60].=1
+    return z_vec
+end
+
+# Array comprehensions
+[RGB(x, 0, 0) for x in 0:0.1:1] # loop from 0 to 1 by 0.1
+
+[RGB(i, j, 0) for i in 0:0.1:1, j in 0:0.1:1] # 2D works as well
